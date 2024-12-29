9796a1e62fa8743b14862a68d42da4b2425c8acd
Shivam Mishra <136583705+mshivam980@users.noreply.github.com>
2024-12-29 20:34:43 +0530

    Delete tests/test_scraper.py

--- tests/test_scraper.py
+++
@@ -1,7 +0,0 @@
-import os
-from scraper.scrape_flipkart import scrape_flipkart_data
-
-def test_scraper():
-    output_file = "output/test_output.csv"
-    scrape_flipkart_data(pages=1, output_file=output_file)
-    assert os.path.exists(output_file), "Output CSV not generated!"
